4ed3d3bdb4702096769d40e31377bbcb12b32f54
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-05-30 09:32:08 +0100

    Agregando espaciosç

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# acl-manager
+# 
+
+<h1 align="center">ACL Manager <img
+src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" height="32" /></h1>
+
+
+
+
+Proyecto para poder gestionar ACLs (Access Control List) en un entorno Linux, al tener muchos comandos repetidos basados en un excel se me ocurrio que seria mas comodo poder formater los comandos de forma automatica usando una interfaz "Amigable".
+
+
+
+<a href="#"><img align="right" src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjB6ZDJzajl6OXVsMW1ydHUxbGEwazE0b3V4b3RncnR0bGNoZzNxeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/tll43JqcmZzgCMLtzM/giphy.gif" width="200 " height="200" /></a>
+
+## ✏️ Dependencias
+
+- 
+
+
--- app/excel_operations.py
+++ app/excel_operations.py
@@ -1,6 +1,25 @@
 import tkinter.font as tkfont
 
+def truncate_path(path, max_length=30):
+    if len(path) <= max_length:
+        return path
+    else:
+        parts = path.split('/')
+        return '.../' + '/'.join(parts[-2:])
+
 def display_data(app):
+    longest_string = ""
+    max_length = 0
+
+    def print_cell_content(item_id, column_name):
+        nonlocal longest_string, max_length
+        cell_content = app.tree.set(item_id, column_name)
+        print(cell_content, " ", len(cell_content))
+
+        if len(cell_content) > max_length:
+            longest_string = cell_content
+            max_length = len(cell_content)
+
     for i in app.tree.get_children():
         app.tree.delete(i)
 
@@ -11,16 +30,22 @@ def display_data(app):
         app.tree.heading(col, text=col)
 
     for row in app.df.to_numpy().tolist():
-        app.tree.insert("", "end", values=row)
+        row[0] = truncate_path(row[0])
+        item_id = app.tree.insert("", "end", values=row)
+        print_cell_content(item_id, app.tree["columns"][0])
 
     font = tkfont.Font()
     for col in app.tree["columns"]:
         max_width = font.measure(col)
+
         for item in app.df[col].astype(str):
             max_width = max(max_width, font.measure(item))
         app.tree.column(col, width=max_width)
+    
+    print("Longest string: ", longest_string)
+    print("Length: ", max_length)
 
 def get_acl(app):
     if app.filepath:
-        print("Consultando ACL...")
+        print("Hola mundo")
         display_data(app)
